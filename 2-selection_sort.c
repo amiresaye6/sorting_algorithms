6754bcbb7e443e6f27fe0c28e063f38d988b464f
Amir Alsayed <amyralsyd367@gmail.com>
2023-09-22 14:52:45 +0300

    selection sort #v3

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -18,7 +18,7 @@ void selection_sort(int *array, size_t size)
 	for (i = 0; i < size; i++)
 	{
 		min_index = i;
-		for (j = i + 1; j < size + 1; j++)
+		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[min_index])
 			{
--- go_life.sh
+++ go_life.sh
@@ -3,5 +3,5 @@
 # execute to add, commit, and push at one step
 
 git add .
-git commit -m "selection sort #v2"
+git commit -m "selection sort #v3"
 git push
