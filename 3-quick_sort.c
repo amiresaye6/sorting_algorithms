8d4370b9e7f1d4d7ff7bf4338cfda36e4a5ea8e5
Amir Alsayed <amyralsyd367@gmail.com>
2023-09-24 17:00:40 +0300

    quick sort

---
+++ 3-quick_sort.c
@@ -0,0 +1,14 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sorting using selection algorithm.
+ * @array: the array to be sorted.
+ * @size: the size of the array.
+ *
+ * Return: Void.
+ */
+void quick_sort(int *array, size_t size)
+{
+    if (size < 2)
+        return;
+}
